0f75a491ec43d3a8620c027349744842915af9f7
Sanchay <sanchay@sanchays-air.mynetworksettings.com>
2026-03-29 16:11:21 -0400

    Added function line comments to validator.py file

--- src/validator.py
+++ src/validator.py
@@ -3,22 +3,27 @@
 def validate_number(value):
     """Validate that value can be converted to a number."""
     try:
+        """Attempt to cast to number, return true if successful"""
         float(value)
         return True
     except (ValueError, TypeError):
+        """Otherwise return false"""
         return False
 
 def validate_operation(op):
     """Validate that operation is supported."""
     valid_ops = ['+', '-', '*', '/']
+    """Return whether operation is part of set of supported operations"""
     return op in valid_ops
 
 def validate_non_negative(n):
     """Validate that a number is non-negative."""
     try:
+        """Return whether number is >= 0"""
         num = float(n)
         return num >= 0
     except (ValueError, TypeError):
+        """Return false if error arose casting to number"""
         return False
         
 def is_positive(n):
